8ea7912eba8ea4a53f6fab041f935712e4299e17
Mohit Kambli <mohitkambli8@gmail.com>
2024-11-17 20:24:29 -0500

    Updated the Type of Redis Port

--- api/services/redis_client.py
+++ api/services/redis_client.py
@@ -5,6 +5,6 @@ from ..config import Config
 # Initialize the Redis client with your configuration
 redis_client = redis.Redis(
     host=Config.REDIS_HOST,
-    port=int(Config.REDIS_PORT),
+    port=12367,
     password=Config.REDIS_PASSWORD
 )
