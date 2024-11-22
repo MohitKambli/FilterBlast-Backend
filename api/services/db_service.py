5a309e0379c29ab6824fe56060b74ed80e210932
Mohit Kambli <mohitkambli8@gmail.com>
2024-11-22 18:07:15 -0500

    Updated the package from sqlalchemy to psycopg2

--- api/services/db_service.py
+++ api/services/db_service.py
@@ -1,25 +1,29 @@
 #from pymongo import MongoClient
 import datetime
 from ..config import Config
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from ..models.logs import Logs
+import psycopg2
 
 '''client = MongoClient(Config.MONGO_URI)
 db = client[Config.MONGO_DB_NAME]
 collection = db[Config.MONGO_COLLECTION_NAME]'''
 
-engine = create_engine(Config.SQLALCHEMY_DATABASE_URI)
-Session = sessionmaker(bind=engine)
-session = Session()
+conn = psycopg2.connect(Config.SQLALCHEMY_DATABASE_URI)
+cur = conn.cursor()
 
 def log_download_entry(user_name, image_name):
     #download_log = {"user_name": user_name, "image_name": image_name, "timestamp": datetime.datetime.now()}
     try:
         #collection.insert_one(download_log)
-        log_entry = Logs(user_name=user_name, image_name=image_name, timestamp=datetime.datetime.now())
-        session.add(log_entry)
-        session.commit()
+        # Insert query with parameterized values
+        cur.execute(
+            '''
+            INSERT INTO logs (user_name, image_name, timestamp)
+            VALUES (%s, %s, %s)
+            ''',
+            (user_name, image_name, datetime.datetime.now())
+        )
+        # Commit the transaction
+        conn.commit()
         return True, "Download logged successfully!"
     except Exception as e:
         return False, str(e)
