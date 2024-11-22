25943415581dc61b797b0ff9b75d9300ec65c1b1
Mohit Kambli <mohitkambli8@gmail.com>
2024-11-22 18:29:26 -0500

    Updated the package from sqlalchemy to psycopg2

--- api/services/db_service.py
+++ api/services/db_service.py
@@ -10,12 +10,28 @@ collection = db[Config.MONGO_COLLECTION_NAME]'''
 conn = psycopg2.connect(Config.SQLALCHEMY_DATABASE_URI)
 cur = conn.cursor()
 
+# Function to create the table if it does not exist
+def create_logs_table():
+    create_table_query = '''
+    CREATE TABLE IF NOT EXISTS logs (
+        id SERIAL PRIMARY KEY,
+        user_name VARCHAR(255) NOT NULL,
+        image_name VARCHAR(255) NOT NULL,
+        timestamp TIMESTAMP NOT NULL
+    )
+    '''
+    cur.execute(create_table_query)
+    conn.commit()  # Ensure the table creation query is executed
+
 def log_download_entry(user_name, image_name):
     #download_log = {"user_name": user_name, "image_name": image_name, "timestamp": datetime.datetime.now()}
     try:
         #collection.insert_one(download_log)
+        
+        # Create the table if it doesn't exist
+        create_logs_table()
+
         # Insert query with parameterized values
-        print('Cursor: ', cur)
         cur.execute(
             '''
             INSERT INTO logs (user_name, image_name, timestamp)
