c9691225f61a5eb65835ed2c3f2e7dbdfcdd71e3
Mohit Kambli <mohitkambli8@gmail.com>
2025-04-02 01:08:48 -0400

    Updated Redis Credentials

--- api/services/redis_client.py
+++ api/services/redis_client.py
@@ -3,7 +3,7 @@ import redis
 
 # Initialize the Redis client with your configuration
 redis_client = redis.Redis(
-    host='redis-15891.c321.us-east-1-2.ec2.redns.redis-cloud.com',
-    port=15891,
-    password='A7NoXEP2t3svaxWCHpKueFqy8IvwrPkC'
+    host='redis-12175.c309.us-east-2-1.ec2.redns.redis-cloud.com',
+    port=12175,
+    password='2oEPdSo1lPCDfNMOV4XBgsxwU4wIKWzT'
 )
